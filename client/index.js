04b2374731ffe9ec023279350a96fed93c18ad0f
HyeonDoonge <migu554@naver.com>
2022-07-11 22:53:27 +0900

    :tada: webpack 환경 설정

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import "@client/index.scss";
+
+(function () {
+  const body = document.body;
+  body.innerHTML = `
+    <div id="root"></div>`;
+})();
--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config();
-const indexRouter = require("../routes/index");
+const indexRouter = require("./routes/index");
 
 const app = express();
 
