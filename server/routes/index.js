cd277716d219bac819592fe25340da578b20834b
pyo-sh <pygosky@gmail.com>
2022-07-12 17:13:44 +0900

    :heavy_plus_sign: mysql2 설치 및 Connection Pool 생성 -#1

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  // res.sendFile(path.join(__dirname, "public", "index.html"));
-  res.render("index.html", {});
-});
 
 module.exports = router;
