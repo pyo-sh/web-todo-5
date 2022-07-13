e814a927b7629949ee8b1ecd2bc4d123b8b5d165
HyeonDoonge <migu554@naver.com>
2022-07-13 16:31:08 +0900

    :sparkles: 전체를 나타내는 컴포넌트 선언 -#3

--- client/App.js
+++ client/App.js
@@ -1,1 +1,14 @@
-export default class App {}
+export default class App {
+  constructor($target) {
+    this.$app = document.createElement("div");
+    this.$app.id = "root";
+    $target.appendChild(this.$app);
+
+    this.render();
+  }
+  render() {
+    this.$app.innerHTML = `
+        <div id="root"></div>
+    `;
+  }
+}
