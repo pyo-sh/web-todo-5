91e7defdb82e379823133985fbf29f9dbc0d39de
pyo-sh <pygosky@gmail.com>
2022-07-13 20:42:23 +0900

    :sparkles: 카드 리스트 UI 작업

--- client/component/Board/Board.js
+++ client/component/Board/Board.js
@@ -1,3 +1,6 @@
+import "@client/component/Board/Board.scss";
+import CardList from "@client/component/Board/CardList";
+
 export default class Board {
   constructor($target) {
     this.$target = $target;
@@ -8,5 +11,8 @@ export default class Board {
     this.$board = document.createElement("div");
     this.$board.className = "board";
     this.$target.appendChild(this.$board);
+
+    // TODO : Render from Lists Data
+    this.cardlist = new CardList(this.$board);
   }
 }
--- client/component/Board/CardList.js
+++ client/component/Board/CardList.js
@@ -1,1 +1,14 @@
-export default class CardList {}
+import "@client/component/Board/CardList.scss";
+
+export default class CardList {
+  constructor($target) {
+    this.$target = $target;
+    this.render();
+  }
+  render() {
+    this.$cardList?.remove();
+    this.$cardList = document.createElement("div");
+    this.$cardList.className = "cardList";
+    this.$target.appendChild(this.$cardList);
+  }
+}
