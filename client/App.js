a73af6baf58dcec816a5c102f763b25d8521ddb7
HyeonDoonge <migu554@naver.com>
2022-07-13 18:48:02 +0900

    :sparkles: 헤더 UI 작업 -#3

--- client/App.js
+++ client/App.js
@@ -1,14 +1,17 @@
+import Header from "@client/component/Header";
+
+// 상태관리를 필요로 하는 덩어리들을 컴포넌트로 만든다.
 export default class App {
   constructor($target) {
-    this.$app = document.createElement("div");
-    this.$app.id = "root";
-    $target.appendChild(this.$app);
-
+    this.$target = $target;
     this.render();
   }
+
   render() {
-    this.$app.innerHTML = `
-        <div id="root"></div>
-    `;
+    this.$app?.remove();
+    this.$app = document.createElement("div");
+    this.$app.id = "root";
+    this.$target.appendChild(this.$app);
+    this.header = new Header(this.$app);
   }
 }
--- client/component/Header.js
+++ client/component/Header.js
@@ -1,1 +1,19 @@
-export default class Header {}
+import "@client/component/header.scss";
+import MenuButton from "@client/component/MenuBar/MenuButton";
+
+export default class Header {
+  constructor($target) {
+    this.$target = $target;
+    this.render();
+  }
+
+  render() {
+    this.$header = document.createElement("header");
+    this.$header.className = "header";
+    this.$header.innerHTML = `
+        <div class="header-title">TO-DO-LIST</div>
+    `;
+    this.$target.appendChild(this.$header);
+    this.menuButton = new MenuButton(this.$header);
+  }
+}
