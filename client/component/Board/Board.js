9768b049d75ef96c54bb9c540caf28ffba51a934
pyo-sh <pygosky@gmail.com>
2022-07-13 20:33:46 +0900

    :sparkles: 보드 UI 작업 -#6

--- client/component/Board/Board.js
+++ client/component/Board/Board.js
@@ -1,1 +1,12 @@
-export default class Board {}
+export default class Board {
+  constructor($target) {
+    this.$target = $target;
+    this.render();
+  }
+  render() {
+    this.$board?.remove();
+    this.$board = document.createElement("div");
+    this.$board.className = "board";
+    this.$target.appendChild(this.$board);
+  }
+}
