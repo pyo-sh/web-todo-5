ac8dc5bd18873a6affed70ead78131b61d88dae8
HyeonDoonge <migu554@naver.com>
2022-07-15 04:35:20 +0900

    :sparkles: 카드 CRUD API - 히스토리 API 연결

--- client/api/card.js
+++ client/api/card.js
@@ -1,23 +1,31 @@
 import { requestCreateHistory } from "@client/api/history";
 // 카드의 변경작업이 발생한 후 호출하면 된다.
-const requestCreateCard = (title, content, author, board) => {
-  return fetch("/card", {
-    method: "PUT",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify({ title, content, author, board }),
-  })
+const requestCreateCard = (title, content, author, boardId, boardName) => {
+  return requestCreateHistory("등록", "", boardName, title, author)
+    .then(() =>
+      fetch("/card", {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, content, author, board: boardId }),
+      }),
+    )
     .then((res) => res.json()) // action, src, dest, target, author
     .catch((err) => {
       console.log("카드 등록 요청 API 에러 발생" + err);
     });
 };
 
-const requestUpdateCard = (id, title, content) => {
-  return fetch(`/card/${id}`, {
-    method: "PATCH",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify({ title, content }),
-  })
+// requestCreateCard("영양제 챙겨먹기", "힘들어요 ㅠ^ㅠ", "team5", 1, "해야할 일");
+
+const requestUpdateCard = (id, title, content, author, boardName) => {
+  return requestCreateHistory("수정", "", boardName, title, author)
+    .then(() =>
+      fetch(`/card/${id}`, {
+        method: "PATCH",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, content }),
+      }),
+    )
     .then((res) => {
       return res.json();
     })
@@ -26,10 +34,13 @@ const requestUpdateCard = (id, title, content) => {
     });
 };
 
-const requestDeleteCard = (id) => {
-  return fetch(`/card/${id}`, {
-    method: "DELETE",
-  })
+const requestDeleteCard = (id, title, author, boardName) => {
+  return requestCreateHistory("삭제", "", boardName, title, author)
+    .then(() =>
+      fetch(`/card/${id}`, {
+        method: "DELETE",
+      }),
+    )
     .then((res) => {
       return res.json();
     })
@@ -38,12 +49,16 @@ const requestDeleteCard = (id) => {
     });
 };
 
-const requestMoveCard = (src, dest, target) => {
-  return fetch(`/move`, {
-    method: "PATCH",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify({ src, dest, target }),
-  })
+// src, dest => boardId, srcName, destName => boardName
+const requestMoveCard = (src, dest, target, srcName, destName, targetName) => {
+  return requestCreateHistory("이동", srcName, destName, targetName, author)
+    .then(() =>
+      fetch(`/move`, {
+        method: "PATCH",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ src, dest, target }),
+      }),
+    )
     .then((res) => {
       return res.json();
     })
